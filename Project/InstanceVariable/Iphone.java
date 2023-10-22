4ae8780ab8aca5890d7227a9c4e59a6ec8f3458f
Shammika Dilshan <31384200+Fordmustangshammika@users.noreply.github.com>
2023-10-22 13:35:36 +0400

    Add files via upload

---
+++ Project/InstanceVariable/Iphone.java
@@ -0,0 +1,13 @@
+public class Iphone {
+
+    String name;  //Instance Variable
+    String color;  //Instance Variable
+    int battery;  //Instance Variable
+    double price;  //Instance Variable
+
+    public void call(){}
+    public void sendText(){}
+    public void findPlaces(){}
+    public void takePhotos(){}
+
+}
